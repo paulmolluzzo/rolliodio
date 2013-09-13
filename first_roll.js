a7aa1eb000ffe450141dd201e853998d51f4274e
Paul Molluzzo <pmolluzzo@gmail.com>
2013-09-13 09:10:52 -0400

    Create games and create new dice in them

--- first_roll.js
+++ first_roll.js
@@ -3,12 +3,26 @@ Games = new Meteor.Collection("games");
 
 if (Meteor.isClient) {
     
-    Template.list.games = function () {
-      return Games.find({}, {sort: {score: -1, name: 1}});
-    };
+    Template.newgame.events({
+        'click input.generate': function(){
+        var currentdate = new Date();
+        var datetime = "Last Sync: " + currentdate.getDate() + "/"+(currentdate.getMonth()+1) 
+        + "/" + currentdate.getFullYear() + " @ " 
+        + currentdate.getHours() + ":" 
+        + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+        Games.insert({name: "test", });
+        var newGame = Games.findOne({name: "test"})
+        console.log(newGame);
+        Dice.insert({type: "d6", sides: 6, game: newGame.name, date: currentdate})
+        console.log(Dice.findOne({game: "test"}))
+        }
+    });
     
     Template.alldice.dice = function () {
-      return Dice.find({});
+      return Dice.find({}, {sort: {date: -1}});
+    };
+    Template.allgames.games = function () {
+      return Games.find({});
     };
     
     
@@ -34,6 +48,10 @@ if (Meteor.isServer) {
       
       // Allows insertion into DB from client console
       Dice.allow({
+        insert: function () { return true; },
+        update: function () { return true; }
+      });
+      Games.allow({
         insert: function () { return true; },
         update: function () { return true; }
       });
